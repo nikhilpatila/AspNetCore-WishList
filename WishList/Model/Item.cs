0a6cd4a79b293011bf07e9afccc94ea5a2fba1d8
Nikhil Patil <nikhilpatila@gmail.com>
2020-02-11 19:05:02 +0530

    Nikhil | COnfigured ApplicationDBContext and a model class

---
+++ WishList/Model/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+    }
+}
